1e0263fb5a5958c2b9cec647b2852970b1482562
burrodecarga <edwinedwinhenriquezh@hotmail.com>
2022-11-07 09:17:33 -0400

    primer commit install tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className='text-2xl font-bold underline text-red-500'>Hello world!</h1>
+    <h1 className='text-3xl font-bold underline text-red-500'>Hello world!</h1>
   )
 }
